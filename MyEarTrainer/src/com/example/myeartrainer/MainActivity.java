f6c10a2b1597832cad35f5f7067f5557ce3ea6c2
Johannes <kenny91@gmx.de>
2014-08-14 15:48:21 +0200

    test

--- MyEarTrainer/src/com/example/myeartrainer/MainActivity.java
+++ MyEarTrainer/src/com/example/myeartrainer/MainActivity.java
@@ -14,7 +14,7 @@ import android.os.Build;
 public class MainActivity extends Activity {
 
 	// Test
-	//PUSHTESTxyyyasdfasd
+	//PUSHTESTxyyyasdfasdasd
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
